0db33b61ea558a3e1b3d846f3fd058c31b5fbf91
Alan <59444140+am26001@users.noreply.github.com>
2022-03-21 18:49:10 -0500

    Update myApp.js

--- JavaScript/myApp.js
+++ JavaScript/myApp.js
@@ -1,1 +1,13 @@
+"use strict";
 
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    document.body.style.backgroundColor = "white";
+}
