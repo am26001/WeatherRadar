e2a21c0d15957a1e9c8dc483f2c928769ef80a53
Alan <59444140+am26001@users.noreply.github.com>
2022-03-27 16:55:14 -0500

    Add files via upload

--- myApp.js
+++ myApp.js
@@ -3,14 +3,16 @@
 
 // 
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
-}
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+  document.getElementById("change").style.backgroundColor = "rgba(0,0,0,0.1)";
 
+
+}
 // 
 function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
-    document.body.style.backgroundColor = "white";
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+  document.body.style.backgroundColor = "white";
 }
