10c4d5c11f7f698ddea7cda889889dab3e42b52e
Alan <59444140+am26001@users.noreply.github.com>
2022-04-05 21:28:34 -0500

    Add files via upload

--- myApp.js
+++ myApp.js
@@ -18,6 +18,8 @@ function openBurgerMenu() {
   document.getElementById("submit").style.color = "rgba(0,0,0,0)";
   // upon opening burger menu, about container becomes white
   document.getElementById("aboutContainer").style.backgroundColor = "rgba(0,0,0,0)";
+  // upon opening burger menu, search bar becomes white
+  document.getElementById("submitBar").style.backgroundColor = "rgba(0,0,0,0)";
 }
 
 // execute upon closing burger menu
@@ -37,4 +39,35 @@ function closeBurgerMenu() {
   document.getElementById("submit").style.color = "rgba(255,255,255,255)";
   // upon opening burger menu, about container returns to its original state
   document.getElementById("aboutContainer").style.backgroundColor = "rgba(0,0,0,0.05)";
+}
+
+/* API Doc: 
+https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key} 
+city name = Denver, API key = 5b2929df6ca27ae27113782edc1615c4 */
+let weather = {
+  apiKey: "5b2929df6ca27ae27113782edc1615c4",
+  fetchWeather: function (city) {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&units=metric&appid=" +
+        this.apiKey
+    )
+      .then((response) => {
+        if (!response.ok) {
+          alert("No weather found.");
+          throw new Error("No weather found.");
+        }
+        return response.json();
+      })
+      .then((data) => this.displayWeather(data));
+  },
+  displayWeather: function (data) {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+    console.log(name,icon,description,temp,humidity,speed);
+    document.querySelector(".homeData").innerHTML = name;
+  }
 }
